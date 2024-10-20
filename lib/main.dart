244b20a42e451febf2d81444c3527ca46e664d0f
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2024-10-20 18:23:37 +0300

    Margin for container

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Container(
           color: Colors.amber,
+          margin: const EdgeInsets.all(15),
           padding: const EdgeInsets.all(25),
           child: const Text(
             "Osama Mohammad Abdalaziz",
