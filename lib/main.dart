1273d5a34a57ff872f6ad154ab66df1ed3233aa8
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2024-10-20 16:11:40 +0300

    use home widget and insert in home Text widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: Text("Osama Mohammad Abdalaziz"),
+    );
   }
 }
