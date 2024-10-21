38a762732bd65bc68f9ad88027f83ba311bef79f
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2024-10-21 21:02:29 +0300

    use Column Widget and insert text,container and images ..etc.

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,34 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.blue,
         ),
-        body: Image.asset(
-          "asset/Images/Dubai.JPG",
-          alignment: Alignment.center,
-          fit: BoxFit.contain,
-          width: 450,
-          height: 450,
+        body: Column(
+          children: [
+            const Text(
+              "Osama Mohammad Abdalaziz",
+              style: TextStyle(
+                fontSize: 20,
+                color: Color.fromARGB(255, 255, 0, 85),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+            ),
+            Image.asset("asset/Images/England.jpg"),
+            Container(
+              width: 150,
+              height: 150,
+              color: const Color.fromARGB(255, 1, 63, 113),
+            ),
+            const Text(
+              "Osama Mohammad Abdalaziz",
+              style: TextStyle(
+                fontSize: 15,
+                color: Color.fromARGB(255, 123, 0, 255),
+              ),
+            ),
+          ],
         ),
       ),
     );
