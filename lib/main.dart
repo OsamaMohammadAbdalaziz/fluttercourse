84e24c9b31483dea68c1e91bcdadde984d2c1b41
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2024-10-21 18:39:10 +0300

    How to use Image widget and insert image.network(from google)

--- lib/main.dart
+++ lib/main.dart
@@ -18,42 +18,8 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.blue,
         ),
-        body: Container(
-          decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: const BorderRadius.all(Radius.circular(50)),
-              border: Border.all(
-                color: Colors.black,
-                width: 10,
-              ),
-              boxShadow: const [
-                BoxShadow(
-                    color: Colors.deepOrangeAccent,
-                    offset: Offset(10, 10),
-                    spreadRadius: 5,
-                    blurRadius: 10,
-                    blurStyle: BlurStyle.normal),
-                BoxShadow(
-                    color: Colors.deepPurpleAccent,
-                    offset: Offset(-10, -10),
-                    spreadRadius: 5,
-                    blurRadius: 10,
-                    blurStyle: BlurStyle.solid),
-              ]),
-          width: 500,
-          height: 400,
-          alignment: Alignment.center,
-          margin: const EdgeInsets.all(15),
-          padding: const EdgeInsets.all(25),
-          child: const Text(
-            "Osama Mohammad Abdalaziz",
-            style: TextStyle(
-                color: Colors.indigo,
-                fontSize: 30,
-                fontWeight: FontWeight.w400,
-                backgroundColor: Colors.red),
-          ),
-        ),
+        body: Image.network(
+            "https://odysseymagazine.com/wp-content/uploads/2024/05/1_private-panoramic-dubai-city-night-view-tour.jpg"),
       ),
     );
   }
