98775a313f53d2db83c99a91f66cc801680d7736
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2024-10-21 21:27:59 +0300

    use SafeArea widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,48 +10,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Flutter Course",
-            style: TextStyle(color: Colors.white),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "Flutter Course",
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blue,
           ),
-          backgroundColor: Colors.blue,
-        ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              "Osama Mohammad",
-              style: TextStyle(
-                fontSize: 10,
-                color: Color.fromARGB(255, 255, 0, 85),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Osama Mohammad",
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color.fromARGB(255, 255, 0, 85),
+                ),
               ),
-            ),
-            Container(
-              width: 50,
-              height: 50,
-              color: Colors.red,
-            ),
-            Image.asset(
-              "asset/Images/England.jpg",
-              width: 150,
-              height: 150,
-            ),
-            Container(
-              width: 20,
-              height: 20,
-              color: const Color.fromARGB(255, 1, 63, 113),
-            ),
-            const Text(
-              "Osama Mohammad",
-              style: TextStyle(
-                fontSize: 10,
-                color: Color.fromARGB(255, 123, 0, 255),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.red,
               ),
-            ),
-          ],
+              Image.asset(
+                "asset/Images/England.jpg",
+                width: 150,
+                height: 150,
+              ),
+              Container(
+                width: 20,
+                height: 20,
+                color: const Color.fromARGB(255, 1, 63, 113),
+              ),
+              const Text(
+                "Osama Mohammad",
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color.fromARGB(255, 123, 0, 255),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
