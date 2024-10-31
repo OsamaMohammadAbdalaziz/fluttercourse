5fbcf3a3174b0c2eba8f4f1b74b02088d6beea18
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2024-10-31 11:21:39 +0300

    When we need to display all the items at once we will be using ListView.builder => include “itemBuilder” & “itemCount”.

--- lib/main.dart
+++ lib/main.dart
@@ -88,46 +88,13 @@ class MyApp extends StatelessWidget {
           ),
           body: Container(
             width: 400,
-            child: ListView(
-              scrollDirection: Axis.vertical,
-              children: [
-                Container(
-                  child: Text(
-                      '${empolyee[0]['First name']} ${empolyee[0]['Middel name']} ${empolyee[0]['Last name']} ${empolyee[0]['Age']} ${empolyee[0]['Country']}'),
-                ),
-                Container(
-                  child: Text(
-                      '${empolyee[6]['First name']} ${empolyee[6]['Middel name']} ${empolyee[6]['Last name']} ${empolyee[6]['Age']} ${empolyee[6]['Country']}'),
-                ),
-                Container(
-                  child: Text(
-                      '${empolyee[3]['First name']} ${empolyee[3]['Middel name']} ${empolyee[3]['Last name']} ${empolyee[3]['Age']} ${empolyee[3]['Country']}'),
-                ),
-                Container(
-                  child: Text(
-                      '${empolyee[4]['First name']} ${empolyee[4]['Middel name']} ${empolyee[4]['Last name']} ${empolyee[4]['Age']} ${empolyee[4]['Country']}'),
-                ),
-                Container(
-                  child: Text(
-                      '${empolyee[1]['First name']} ${empolyee[1]['Middel name']} ${empolyee[1]['Last name']} ${empolyee[1]['Age']} ${empolyee[1]['Country']}'),
-                ),
-                Container(
-                  child: Text(
-                      '${empolyee[7]['First name']} ${empolyee[7]['Middel name']} ${empolyee[7]['Last name']} ${empolyee[7]['Age']} ${empolyee[7]['Country']}'),
-                ),
-                Container(
-                  child: Text(
-                      '${empolyee[8]['First name']} ${empolyee[8]['Middel name']} ${empolyee[8]['Last name']} ${empolyee[8]['Age']} ${empolyee[8]['Country']}'),
-                ),
-                Container(
-                  child: Text(
-                      '${empolyee[5]['First name']} ${empolyee[5]['Middel name']} ${empolyee[5]['Last name']} ${empolyee[5]['Age']} ${empolyee[5]['Country']}'),
-                ),
-                Container(
-                  child: Text(
-                      '${empolyee[2]['First name']} ${empolyee[2]['Middel name']} ${empolyee[2]['Last name']} ${empolyee[2]['Age']} ${empolyee[2]['Country']}'),
-                ),
-              ],
+            child: ListView.builder(
+              itemCount: empolyee.length,
+              itemBuilder: (context, i) {
+                return Container(
+                  child: Text('${empolyee[i]}'),
+                );
+              },
             ),
           ),
         ),
