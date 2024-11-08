30f8de158f7b4c02c33a71cf509a3a1c9c69db17
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2024-11-08 19:15:46 +0200

    -	Other properties for ListTile Widget: (Leading, tileColor, isThreeLine, Enabled, onLongPress) -	Other properties for Card Widget: (Elevation, Margin, Shape)

--- lib/main.dart
+++ lib/main.dart
@@ -26,15 +26,108 @@ class MyApp extends StatelessWidget {
                   child: Container(
                     padding: EdgeInsets.all(20),
                     child: ListTile(
-                      onTap: () {
-                        print("On Tap");
+                      tileColor: Colors.blue,
+                      leading: Text(
+                        "1",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w900),
+                      ),
+                      onLongPress: () {
+                        print("Long Press");
                       },
-                      title: Text("Title"),
-                      subtitle: Text("Subtitle"),
-                      trailing: Text("Trailing"),
+                      onTap: () {
+                        print("Osama");
+                      },
+                      title: Text(
+                        "Osama",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w900),
+                      ),
+                      subtitle: Text(
+                        "Age:22",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w900),
+                      ),
+                      trailing: Text(
+                        "Cyber Secuirty 2025",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w900),
+                      ),
                     ),
                   ),
-                )
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(150)),
+                  margin: EdgeInsets.only(top: 20, bottom: 20),
+                  elevation: 0.0,
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: ListTile(
+                      isThreeLine: true,
+                      enabled: false,
+                      tileColor: Colors.black,
+                      leading: Text(
+                        "2",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white),
+                      ),
+                      onTap: () {
+                        print("Ahmed");
+                      },
+                      title: Text(
+                        "Ahmed",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white),
+                      ),
+                      subtitle: Text(
+                        "Age:24",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white),
+                      ),
+                      trailing: Text(
+                        "Civil Engineer 2023",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: ListTile(
+                      tileColor: Colors.red,
+                      leading: Text("3"),
+                      onTap: () {
+                        print("Bayan");
+                      },
+                      title: Text(
+                        "Bayan",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w900),
+                      ),
+                      subtitle: Text(
+                        "Age:22",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w900),
+                      ),
+                      trailing: Text(
+                        "IT 2025",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w900),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
