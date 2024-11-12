99d59fa792e16ced6706c7fe4d0b882510d3e1aa
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2024-11-13 00:03:58 +0200

    How to use Icon widget and give methods for any icon

--- lib/main.dart
+++ lib/main.dart
@@ -14,121 +14,16 @@ class MyApp extends StatelessWidget {
         child: Scaffold(
           appBar: AppBar(
             title: const Text(
-              "Card and ListTile",
+              "Icon & Icon button",
               style: TextStyle(color: Colors.white),
             ),
             backgroundColor: Colors.blue,
           ),
           body: Container(
-            child: ListView(
-              children: [
-                Card(
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    child: ListTile(
-                      tileColor: Colors.blue,
-                      leading: Text(
-                        "1",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w900),
-                      ),
-                      onLongPress: () {
-                        print("Long Press");
-                      },
-                      onTap: () {
-                        print("Osama");
-                      },
-                      title: Text(
-                        "Osama",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w900),
-                      ),
-                      subtitle: Text(
-                        "Age:22",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w900),
-                      ),
-                      trailing: Text(
-                        "Cyber Secuirty 2025",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w900),
-                      ),
-                    ),
-                  ),
-                ),
-                Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(150)),
-                  margin: EdgeInsets.only(top: 20, bottom: 20),
-                  elevation: 0.0,
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    child: ListTile(
-                      isThreeLine: true,
-                      enabled: false,
-                      tileColor: Colors.black,
-                      leading: Text(
-                        "2",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w900,
-                            color: Colors.white),
-                      ),
-                      onTap: () {
-                        print("Ahmed");
-                      },
-                      title: Text(
-                        "Ahmed",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w900,
-                            color: Colors.white),
-                      ),
-                      subtitle: Text(
-                        "Age:24",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w900,
-                            color: Colors.white),
-                      ),
-                      trailing: Text(
-                        "Civil Engineer 2023",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w900,
-                            color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                Card(
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    child: ListTile(
-                      tileColor: Colors.red,
-                      leading: Text("3"),
-                      onTap: () {
-                        print("Bayan");
-                      },
-                      title: Text(
-                        "Bayan",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w900),
-                      ),
-                      subtitle: Text(
-                        "Age:22",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w900),
-                      ),
-                      trailing: Text(
-                        "IT 2025",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w900),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+            child: Icon(
+              Icons.account_balance_wallet,
+              color: Colors.red,
+              size: 150,
             ),
           ),
         ),
