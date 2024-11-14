7ace3478f676d00bdeb78a22eab7249adaa13996
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2024-11-14 19:37:20 +0200

    BorderRadius in Container widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,139 +14,36 @@ class MyApp extends StatelessWidget {
         child: Scaffold(
           appBar: AppBar(
             title: const Text(
-              "Example",
+              "Flutter Course",
               style: TextStyle(color: Colors.white),
             ),
             backgroundColor: Colors.blue,
           ),
           body: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      color: const Color(0xffe6f0fa),
-                      border: Border.all(color: Colors.black, width: 1)),
-                  padding: const EdgeInsets.all(10),
-                  width: 1000,
-                  child: Text(
-                    "Osama Elmahdi",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 20,
-                        color: Colors.grey[700]),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: const Color(0xffe6f0fa),
-                      border: Border.all(color: Colors.black, width: 1)),
-                  padding: const EdgeInsets.all(10),
-                  margin: const EdgeInsets.only(top: 10),
-                  width: 1000,
-                  child: Text(
-                    "One of the most common layout patterns is to arrange widgets vertically or horizontally. You can use a Row widget to arrange widgets horizontally and a Column widget to arrange widgets vertically.",
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.grey[700],
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: const Color(0xffe6f0fa),
-                      border: Border.all(color: Colors.black, width: 1)),
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  margin: const EdgeInsets.only(top: 10),
-                  width: 1000,
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.star, size: 20),
-                          Icon(Icons.star, size: 20),
-                          Icon(Icons.star, size: 20),
-                          Icon(Icons.star, size: 20),
-                          SizedBox(
-                            width: 150,
-                          ), // Add space between the icons and the text
-                          Text(
-                            "170 Reviews",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: const Color(0xffe6f0fa),
-                      border: Border.all(color: Colors.black, width: 1)),
-                  padding: const EdgeInsets.all(10),
-                  margin: const EdgeInsets.only(top: 11),
-                  width: 1000,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      const Spacer(),
-                      Column(
-                        children: [
-                          Icon(Icons.person, color: Colors.green[400]),
-                          const Text(
-                            "Prep",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          Container(
-                            height: 8,
-                          ),
-                          const Text(
-                            "25 min",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      Column(
-                        children: [
-                          Icon(Icons.alarm, color: Colors.green[400]),
-                          const Text(
-                            "Cook",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          Container(
-                            height: 8,
-                          ),
-                          const Text(
-                            "1 hr",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      Column(
-                        children: [
-                          Icon(Icons.restaurant, color: Colors.green[400]),
-                          const Text(
-                            "Feed",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          Container(
-                            height: 8,
-                          ),
-                          const Text(
-                            "4-6",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                    ],
-                  ),
-                )
-              ],
+            decoration: BoxDecoration(
+                color: Colors.cyan,
+                borderRadius: const BorderRadius.all(Radius.circular(4000)),
+                border: Border.all(color: Colors.black, width: 10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    spreadRadius: 10,
+                    offset: Offset(10, 10),
+                  )
+                ]),
+            width: 500,
+            height: 400,
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(20),
+            child: const Text(
+              "Osama Elmahdi",
+              style: TextStyle(
+                color: Colors.indigo,
+                fontSize: 40,
+                fontWeight: FontWeight.w700,
+              ),
             ),
           ),
         ),
