131b9a442f2d57b0a3155b3a322971c8bd2d561f
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2024-12-09 05:32:20 +0200

    To create a Material Design button we need to import MaterialButton.

--- lib/main.dart
+++ lib/main.dart
@@ -20,30 +20,23 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.blue,
           ),
           body: Container(
-            decoration: BoxDecoration(
-                color: Colors.cyan,
-                borderRadius: const BorderRadius.all(Radius.circular(4000)),
-                border: Border.all(color: Colors.black, width: 10),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 10,
-                    spreadRadius: 10,
-                    offset: Offset(10, 10),
-                  )
-                ]),
-            width: 500,
-            height: 400,
-            alignment: Alignment.center,
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              "Osama Elmahdi",
-              style: TextStyle(
-                color: Colors.indigo,
-                fontSize: 40,
-                fontWeight: FontWeight.w700,
-              ),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                MaterialButton(
+                  color: Colors.black,
+                  textColor: Colors.white,
+                  minWidth: 110,
+                  height: 100,
+                  onLongPress: () {
+                    print("on Long Press Osama");
+                  },
+                  onPressed: () {
+                    print("Osama Elmahdi");
+                  },
+                  child: Text("Osama Elmahdi"),
+                )
+              ],
             ),
           ),
         ),
